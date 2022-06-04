dae1c8889a92ba30d99f190cdebabbe137ddc44f
khalid <khleadini@gmail.com>
2022-06-04 06:27:01 +0300

    Liking Likes

---
+++ script.js
@@ -0,0 +1,26 @@
+function incrementButton(){
+    var element = document.getElementById('incrementText');
+    var value = element.innerHTML;
+    ++value
+    console.log(value);
+    document.getElementById('incrementText').innerHTML = value;
+}
+
+function incrementButton1(){
+    var element = document.getElementById('incrementText1');
+    var value = element.innerHTML;
+    ++value
+    console.log(value);
+    document.getElementById('incrementText1').innerHTML = value;
+}
+
+
+function incrementButton2(){
+    var element = document.getElementById('incrementText2');
+    var value = element.innerHTML;
+    ++value
+    console.log(value);
+    document.getElementById('incrementText2').innerHTML = value;
+}
+
+
